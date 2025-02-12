a8045933b59340847a1678a1d010fffac7d7dfc4
johnyesit <ziao.ye@nih.gov>
2025-02-12 17:09:32 -0500

    Update env variables

--- env/master.sh
+++ env/master.sh
@@ -1,5 +1,6 @@
-export CCDI_CBIO_SITE_URL="${CCDI_CBIO_SITE_URL:-http://localhost:9000}"
-export CBIOPORTAL_URL="${CBIOPORTAL_URL:-http://localhost:8080}"
-# export CBIOPORTAL_URL="${CBIOPORTAL_URL:-https://www.cbioportal.org}"
+# export CCDI_CBIO_SITE_URL="${CCDI_CBIO_SITE_URL:-http://localhost:9000}"
+export CCDI_CBIO_SITE_URL="${CCDI_CBIO_SITE_URL:-https://cbiit.github.io/ccdi-cbio-site}"
+# export CBIOPORTAL_URL="${CBIOPORTAL_URL:-http://localhost:8080}"
+export CBIOPORTAL_URL="${CBIOPORTAL_URL:-https://www.cbioportal.org}"
 export GENOME_NEXUS_URL="${GENOME_NEXUS_URL:-https://www.genomenexus.org}"
 export BACKEND=cbioportal:demo-fix-clinical-table-sorting
