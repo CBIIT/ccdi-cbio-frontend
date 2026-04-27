396948dfc4a665d22a187f3d1d0f4119a02aa0cc
johnyesit <ziao.ye@nih.gov>
2026-04-27 14:59:47 -0400

    Replace Link with anchor tag with setting absolute URL

--- src/shared/components/StudyLink/StudyLink.tsx
+++ src/shared/components/StudyLink/StudyLink.tsx
@@ -1,24 +1,49 @@
 import * as React from 'react';
-import { CancerStudy } from 'cbioportal-ts-api-client';
-import { Link } from 'react-router-dom';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { buildCBioPortalPageUrl } from '../../../shared/api/urls';
 
-export class StudyLink extends React.Component<
-    {
-        studyId: string;
-        className?: string;
-        studyName?: string;
-    },
+type StudyLinkProps = {
+    studyId: string;
+    className?: string;
+    studyName?: string;
+};
+
+class StudyLinkComponent extends React.Component<
+    StudyLinkProps & RouteComponentProps,
     {}
 > {
+    private handleClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+        if (
+            event.defaultPrevented ||
+            event.button !== 0 ||
+            event.metaKey ||
+            event.altKey ||
+            event.ctrlKey ||
+            event.shiftKey
+        ) {
+            return;
+        }
+
+        event.preventDefault();
+        this.props.history.push(`/study?id=${this.props.studyId}`);
+    };
+
     render() {
+        const absoluteStudyUrl = buildCBioPortalPageUrl('study', {
+            id: this.props.studyId,
+        });
+
         return (
-            <Link
-                to={`/study?id=${this.props.studyId}`}
+            <a
+                href={absoluteStudyUrl}
                 className={this.props.className}
+                onClick={this.handleClick}
                 aria-label={`Open the study summary page for ${this.props.studyName} in a new tab`}
             >
                 {this.props.children}
-            </Link>
+            </a>
         );
     }
 }
+
+export const StudyLink = withRouter(StudyLinkComponent);
